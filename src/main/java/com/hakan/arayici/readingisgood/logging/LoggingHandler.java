33fa3eb4ced9220b10cc1c822409008ccdd347d9
hakan <hakanarayici1@hotmail.com>
2022-02-25 08:33:13 +0300

    logging aspect

---
+++ src/main/java/com/hakan/arayici/readingisgood/logging/LoggingHandler.java
@@ -0,0 +1,60 @@
+package com.hakan.arayici.readingisgood.logging;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.CodeSignature;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingHandler {
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.RequestMapping)")
+    public void pointcut() {
+    }
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Before("pointcut()")
+    public void logMethod(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        RequestMapping mapping = signature.getMethod().getAnnotation(RequestMapping.class);
+
+        Map<String, Object> parameters = getParameters(joinPoint);
+
+        try {
+            log.info("==> path(s): {}, method(s): {}, arguments: {} ",
+                    mapping.path(), mapping.method(), mapper.writeValueAsString(parameters));
+        } catch (JsonProcessingException e) {
+            log.error("Error while converting", e);
+        }
+    }
+
+    private Map<String, Object> getParameters(JoinPoint joinPoint) {
+        CodeSignature signature = (CodeSignature) joinPoint.getSignature();
+
+        Map<String, Object> map = new HashMap<>();
+
+        String[] parameterNames = signature.getParameterNames();
+
+        for (int i = 0; i < parameterNames.length; i++) {
+            map.put(parameterNames[i], joinPoint.getArgs()[i]);
+        }
+
+        return map;
+    }
+
+}
